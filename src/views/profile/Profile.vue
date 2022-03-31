752d42490c0a22fcb92456bfb58ae7d4f34ecb5f
rss <641270025@qq.com>
2022-03-31 17:39:59 +0800

    test profile

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    <h3>This is Profile page .</h3>
+  <div class="Profile">
+    <h3>学习参考项目 </h3>
+    </br>
+
+    <p><span class="sp1">e-mail:</span> forever.r@foxmail.com</p>
+    </br>
+    <p><span class="sp1">QQ:</span> 641270025</p>
   </div>
 
 </template>
 
 <script>
   export default {
-    name:"Profile"
+    name: "Profile"
   }
 </script>
 
 <style scoped>
+  .Profile {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .sp1 {
+    font-weight: bold;
+    font-size: 17px;
+  }
 </style>
